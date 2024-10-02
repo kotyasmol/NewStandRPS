3792c57b5257a64ff42be582f77f3a0c2d897541
Kate Vyshenskaia <lmaoyyyya@gmail.com>
2024-10-02 15:23:20 +0500

    Добавьте файлы проекта.

---
+++ StandRTL/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using HandyControl.Themes;
+using Stylet;
+using StyletIoC;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using NewStandRPS.ViewModels;
+
+namespace NewStandRPS
+{
+    public class Bootstrapper : Bootstrapper<MainViewModel>
+    {
+        private readonly string _programName = "Realtek Stand";
+        protected override void ConfigureIoC(IStyletIoCBuilder builder)
+        {
+                 
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appDir = Path.Combine(appDataPath, _programName);
+            Directory.CreateDirectory(appDir);
+
+        }
+
+        protected override void Configure()
+        {                       
+
+            base.Configure();
+        }
+    }
+}
