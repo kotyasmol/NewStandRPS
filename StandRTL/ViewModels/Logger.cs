91181a8c58e9d61aee9d001a0ed6a56490e091f1
Kate Vyshenskaia <lmaoyyyya@gmail.com>
2024-10-07 11:25:19 +0500

    уууу

--- StandRTL/ViewModels/Logger.cs
+++ StandRTL/ViewModels/Logger.cs
@@ -3,7 +3,8 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Threading;
+using Serilog;
+using Serilog.Events;
 
 namespace NewStandRPS.ViewModels
 {
@@ -16,25 +17,56 @@ namespace NewStandRPS.ViewModels
     public class Logger
     {
         private ObservableCollection<LogEntry> _logMessages;
-        private string _logFilePath;
         private bool _useDarkTheme = false; // Переключение темы 
+
+        // Serilog Logger
+        private Serilog.Core.Logger _serilogLogger;
 
         public Logger(ObservableCollection<LogEntry> logMessages, string logFilePath)
         {
             _logMessages = logMessages ?? new ObservableCollection<LogEntry>();
-            _logFilePath = logFilePath;
+
+            // Инициализация Serilog
+            _serilogLogger = new LoggerConfiguration()
+                .MinimumLevel.Debug() // Минимальный уровень логирования
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7, fileSizeLimitBytes: 10_000_000) // Логирование в файл с ежедневной ротацией
+                .CreateLogger();
         }
 
         public void Log(string message, LogLevel level)
         {
             string timestamp = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
-            string logEntry = $"{timestamp} [{level}] {message}";
+            string logEntry = $"{timestamp}  {message}";
 
-            // Запись в файл
-            WriteLogToFile(logEntry);
+            // Используем Serilog для записи в файл
+            switch (level)
+            {
+                case LogLevel.Error:
+                    _serilogLogger.Error(message);
+                    break;
+                case LogLevel.Info:
+                    _serilogLogger.Information(message);
+                    break;
+                case LogLevel.Warning:
+                    _serilogLogger.Warning(message);
+                    break;
+                case LogLevel.Debug:
+                    _serilogLogger.Debug(message);
+                    break;
+                case LogLevel.Critical:
+                    _serilogLogger.Fatal(message);
+                    break;
+                case LogLevel.Success:
+                    _serilogLogger.Information(message); // Serilog не имеет "Success", используем Information
+                    break;
+                default:
+                    _serilogLogger.Information(message);
+                    break;
+            }
 
-            // Добавление лога в коллекцию с цветом
             SolidColorBrush color = GetLogColor(level);
+
+            // Добавляем сообщение в коллекцию для отображения в GUI
             Application.Current.Dispatcher.Invoke(() =>
             {
                 _logMessages.Add(new LogEntry
@@ -45,33 +77,18 @@ namespace NewStandRPS.ViewModels
             });
         }
 
-        private void WriteLogToFile(string logEntry)
-        {
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(_logFilePath, true))
-                {
-                    sw.WriteLine(logEntry);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка записи лога: {ex.Message}");
-            }
-        }
-
         private SolidColorBrush GetLogColor(LogLevel level)
         {
             if (_useDarkTheme)
             {
                 return level switch
                 {
-                    LogLevel.Error => new SolidColorBrush(Colors.Red),   
-                    LogLevel.Info => new SolidColorBrush(Colors.Black),  
-                    LogLevel.Warning => new SolidColorBrush(Colors.Orange), 
-                    LogLevel.Debug => new SolidColorBrush(Colors.Gray),  
-                    LogLevel.Critical => new SolidColorBrush(Colors.DarkRed), 
-                    LogLevel.Success => new SolidColorBrush(Colors.Green),  
+                    LogLevel.Error => new SolidColorBrush(Colors.Red),
+                    LogLevel.Info => new SolidColorBrush(Colors.Black),
+                    LogLevel.Warning => new SolidColorBrush(Colors.Orange),
+                    LogLevel.Debug => new SolidColorBrush(Colors.Gray),
+                    LogLevel.Critical => new SolidColorBrush(Colors.DarkRed),
+                    LogLevel.Success => new SolidColorBrush(Colors.Green),
                     _ => new SolidColorBrush(Colors.Black),           // По умолчанию черный
                 };
             }
@@ -79,16 +96,17 @@ namespace NewStandRPS.ViewModels
             {
                 return level switch
                 {
-                    LogLevel.Error => new SolidColorBrush(Colors.Red),   
-                    LogLevel.Info => new SolidColorBrush(Colors.White),  
-                    LogLevel.Warning => new SolidColorBrush(Colors.Yellow), 
-                    LogLevel.Debug => new SolidColorBrush(Colors.White), 
-                    LogLevel.Critical => new SolidColorBrush(Colors.DarkRed), 
-                    LogLevel.Success => new SolidColorBrush(Colors.Lime), 
+                    LogLevel.Error => new SolidColorBrush(Colors.Red),
+                    LogLevel.Info => new SolidColorBrush(Colors.White),
+                    LogLevel.Warning => new SolidColorBrush(Colors.Yellow),
+                    LogLevel.Debug => new SolidColorBrush(Colors.White),
+                    LogLevel.Critical => new SolidColorBrush(Colors.DarkRed),
+                    LogLevel.Success => new SolidColorBrush(Colors.Lime),
                     _ => new SolidColorBrush(Colors.White),            // По умолчанию белый для темной темы
                 };
             }
         }
+
         public enum LogLevel
         {
             Error,    // Ошибка
